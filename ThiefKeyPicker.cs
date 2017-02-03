8e4cd05273da42c96ec9a6981924e69550adbf15
renegatec0de <max0malygin@gmail.com>
2017-02-02 21:28:30 -0500

    Add files via upload

---
+++ ThiefKeyPicker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public class ThiefKeyPicker
+    {
+
+        protected readonly House targetedHouseForBreakIn = null;
+
+        public ThiefKeyPicker()
+        {
+
+
+        }
+
+    }
+}
